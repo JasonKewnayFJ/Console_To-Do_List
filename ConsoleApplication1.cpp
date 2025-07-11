0431bb326e84bc86cf452e832eba69f3954132d3
Jay <jasonkenway.fj@gmail.com>
2025-07-11 18:46:50 +0300

    Добавить обработку ввода и функцию toLower
    Добавлена функция для преобразования строк в нижний регистр. Изменено условие выхода из программы: теперь принимаются "no" и "exit". Также добавлена проверка на пустоту вектора, чтобы выводить сообщение, если пользователь не ввел ничего.

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -2,11 +2,16 @@
 #include <ctime>
 #include <vector>  
 #include <string>
+#include <algorithm>
 
+	std::string toLower(std::string s) {
+		std::transform(s.begin(), s.end(), s.begin(), ::tolower);
+		return s;
+	}
 	int main()
 	{
 		std::vector<std::string> v;
-			std::cout << "Enter exit to exit: " << std::endl;
+			std::cout << "Enter no or exit to exit: " << std::endl;
 			std::cout << "..." << std::endl;
 			std::cout << "Enter what do you need to do?" << std::endl;
 			std::cout << "..." << std::endl;
@@ -17,14 +22,19 @@
 			std::getline(std::cin, input);
 
 
-			if (input == "No")
+			if (toLower(input) == "no" || toLower(input) == "exit")
 			{
+				if (v.empty()) {
+					std::cout << "You didn't enter anything." << std::endl;
+					return 0;
+				}
 				std::cout << "Here's the list..." << std::endl;
 				for (std::string str : v) {
 					std::cout << str << std::endl;
 				}
 					return 0;
 			}
+
 			else {
 
 				std::cout << "Something else?" << std::endl;
@@ -32,5 +42,9 @@
 			}
 		}
 
+
+
 	}
+
+
 

a79ee62a3ffd3bfd066530a721ae4d99a823a1f7
Jay <jasonkenway.fj@gmail.com>
2025-07-11 17:37:15 +0300

    Добавить ввод пользователя и обработку команд
    Изменены заголовочные файлы для использования `<ctime>`, `<vector>`, и `<string>`.
    Основная функция `main` теперь содержит бесконечный цикл, который запрашивает ввод пользователя.
    При вводе "No" программа выводит список введенных строк и завершает выполнение.
    В противном случае введенная строка добавляется в вектор `v`, и программа продолжает запрашивать ввод.

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,7 +1,36 @@
 ﻿#include <iostream>
+#include <ctime>
+#include <vector>  
+#include <string>
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+	int main()
+	{
+		std::vector<std::string> v;
+			std::cout << "Enter exit to exit: " << std::endl;
+			std::cout << "..." << std::endl;
+			std::cout << "Enter what do you need to do?" << std::endl;
+			std::cout << "..." << std::endl;
+
+		while (true) 
+		{
+			std::string input;
+			std::getline(std::cin, input);
+
+
+			if (input == "No")
+			{
+				std::cout << "Here's the list..." << std::endl;
+				for (std::string str : v) {
+					std::cout << str << std::endl;
+				}
+					return 0;
+			}
+			else {
+
+				std::cout << "Something else?" << std::endl;
+					v.push_back(input);
+			}
+		}
+
+	}
 

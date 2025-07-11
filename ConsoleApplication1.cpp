cd7a355dbcd0197f6af2be278c062bfa7ad377d2
Jay <jasonkenway.fj@gmail.com>
2025-07-11 19:01:10 +0300

    Добавление поддержки JSON и улучшение логики программы
    Внедрены библиотеки `nlohmann/json.hpp` и `fstream` для работы с JSON и файловой системой. Установлена локаль на русский язык. Реализовано создание объекта JSON из вектора строк и сохранение данных в файл `data.json` с отступами. Оптимизирован цикл по вектору строк с использованием константной ссылки.
    
    Обновлены файлы проекта: добавлен `packages.config` для управления зависимостями, а также условия для импорта необходимых файлов сборки. Добавлены примерные данные в `data.json` и информация о пакете `nlohmann.json` в `packages.config`.

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -3,6 +3,10 @@
 #include <vector>  
 #include <string>
 #include <algorithm>
+#include <nlohmann/json.hpp>
+#include <fstream> 
+
+using json = nlohmann::json;
 
 	std::string toLower(std::string s) {
 		std::transform(s.begin(), s.end(), s.begin(), ::tolower);
@@ -10,10 +14,13 @@
 	}
 	int main()
 	{
+		setlocale(LC_ALL, "Russian");
 		std::vector<std::string> v;
-			std::cout << "Enter no or exit to exit: " << std::endl;
-			std::cout << "..." << std::endl;
+		json j = v;
+
 			std::cout << "Enter what do you need to do?" << std::endl;
+			std::cout << "..." << std::endl;
+			std::cout << "Enter no or exit to exit: " << std::endl;
 			std::cout << "..." << std::endl;
 
 		while (true) 
@@ -28,17 +35,31 @@
 					std::cout << "You didn't enter anything." << std::endl;
 					return 0;
 				}
+
 				std::cout << "Here's the list..." << std::endl;
-				for (std::string str : v) {
+				for (const std::string& str : v) {
 					std::cout << str << std::endl;
 				}
-					return 0;
+
+				json j = v; // обновляем JSON перед сохранением
+
+				std::ofstream file("data.json");
+				if (!file.is_open()) {
+					std::cout << "Ошибка открытия файла\n";
+					return 1;
+				}
+
+				file << j.dump(4); // записываем с отступами
+				std::cout << "Данные сохранены\n";
+				return 0;
 			}
+
 
 			else {
 
 				std::cout << "Something else?" << std::endl;
 					v.push_back(input);
+
 			}
 		}
 
